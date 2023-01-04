299351e634b2d913bc91dbc61f040ba0b13217df
gareth5james <gareth5james@yahoo.co.uk>
2023-01-04 22:12:50 +0000

    day 3 started

---
+++ js_files/day3.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const binaryList = fs
+  .readFileSync(`${__dirname}/../text_files/day3.txt`, "utf-8")
+  .split("\n");
+
+console.log(binaryList);
+
+function task1(binaryList) {
+  const gamma = "";
+  const epsilon = "";
+
+for(let i = 0)
+}
